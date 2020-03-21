3efbdf3f6571cba29c892bc3fe1148ab3da5aece
abricq <arthur.bricq@epfl.ch>
2020-03-21 11:23:02 +0100

    Second plot included

--- VirusAnalysis.py
+++ VirusAnalysis.py
@@ -58,78 +58,83 @@ def getDateIndex(date):
     return np.argwhere(confirmed.columns == date)[0,0]-4
 
 
-#%% Plots
 
-plt.figure()
-plt.plot(getInfected('France',getDateIndex('2/22/20')),'x',label='France')
-plt.plot(getInfected('Italy',getDateIndex('2/22/20')),'x',label='Italy')
-plt.xlabel('Number of days after February 22th')
-plt.ylabel('Number of infected persons')
-plt.legend()
-
-
-#%% data to be used  
+#%% data to be used to generate the plots
 
 countries = ['Italy','France','Switzerland','Spain','Netherlands']
 populations = [60.5e6, 67e6, 8.6e6, 46.6e6, 17.2e6]
-n_countries = [0,8,5,5,9]
+n_countries = [0,9,3,5,10]
 date = '2/22/20'
 
-
-
-
-#%% 
 # For the ongoing part, I want to compare one european country with Italy, which can be quite tricky
 # One one plot, we are going to show Italy, the country X, and the country X shifted by n_x days 
 
-countryIndex = 4
-country = countries[countryIndex]
-n = n_countries[countryIndex]
-y_italy = getInfected('Italy',getDateIndex(date)) / populations[0]
-y_country = getInfected(country,getDateIndex(date)) / populations[countryIndex]
-plt.figure()
-# Italy is the reference 
-plt.plot(y_italy,'x',label='Italy')
-# Plot the second country
-plt.plot(y_country,'x',label=country)
-plt.plot(y_country[n:],'x-.',label=country + ' shifted by ' + str(n) + ' days')
-plt.xlabel('Number of days after ' + date)
-plt.ylabel('Ratio of infected persons')
-plt.title('Italy versus ' + country + '  (normalized by population) \nDays lagging behind Italy: ' + str(n))
-plt.legend()
+#%% Plot 1: normalised infection in different countries 
 
-#%% Make of this a nice plot that recap this situation in europe
+fig1, axs = plt.subplots(2,2,figsize = (10,12))
+axs = axs.ravel()
 
-title = 'Analysis of Covid-19 for different european countries \n' 
-title += 'Normalised populations are used to compare the progress of the virus.\n'
-title += 'Italy is used as the reference to compare other european countries.\n'
-title += 'Each country is shifted by an amount of days and then compared with Italy.\n'
-title += 'A. Bricq & J. Felisaz - updated on ' + confirmed.columns[-1]
+for i, ax in enumerate(axs):
 
-fig = plt.figure(figsize = (15,18))
-#fig.suptitle('bold figure suptitle', fontsize=14, fontweight='bold')
-plt.figtext(0.1, 0.98, title, fontsize = 20, linespacing = 2)
+    country = countries[i+1]
+    n = n_countries[i+1]
+    y_italy = getInfected('Italy',getDateIndex(date)) / populations[0]
+    y_country = getInfected(country,getDateIndex(date)) / populations[i+1]
+    
+    ax.plot(y_italy,'x',label='Italy')
+    ax.plot(y_country,'x',label=country)
+    ax.plot(y_country[n:],'x-.',label=country + ' shifted by ' + str(n) + ' days')
+    ax.set_xlabel('Number of days after ' + date)
+    ax.set_ylabel('Ratio of infected persons')
+    ax.set_title('Italy versus ' + country + ' \nDays lagging behind Italy: ' + str(n) + '\nNumber of cases : ' + str(getNumberOfInfectedForCountry(country)))
+    ax.legend()
+    
+fig1.subplots_adjust(wspace=0.3,hspace=0.4)
+fig1.suptitle('COVID-19 Comparison of different normalised european countries \nDate : '+ confirmed.columns[-1])
 
-for i in range(1,len(countries)):
+    
+#%% Plot 2: number of new cases and cumulative cases for different european countries
 
+numberOfDays = 10 
+
+
+fig2, axs = plt.subplots(2,2,figsize = (10,10))
+axs = axs.ravel()
+for i, ax in enumerate(axs):
+    
     country = countries[i]
     n = n_countries[i]
-    y_italy = getInfected('Italy',getDateIndex(date)) / populations[0]
-    y_country = getInfected(country,getDateIndex(date)) / populations[i]
+    cumulative = getInfected(country,getDateIndex(date)) 
+    newCases = cumulative[1:]-cumulative[:-1]
     
-    plt.subplot(2,2,i)
-    plt.plot(y_italy,'x',label='Italy')
-    plt.plot(y_country,'x',label=country)
-    plt.plot(y_country[n:],'x-.',label=country + ' shifted by ' + str(n) + ' days')
-    plt.xlabel('Number of days after ' + date)
-    plt.ylabel('Ratio of infected persons')
-    plt.title('Italy versus ' + country + ' \nDays lagging behind Italy: ' + str(n))
-    plt.legend()
+    x = range(len(cumulative))
+    fit = np.polyfit(np.log(x[-numberOfDays:]),np.log(cumulative[-numberOfDays:]),1)
+    print(fit)
     
+    ax.set_title(country + '  (' + str(getNumberOfInfectedForCountry(country)) + ' cases)')
+    ax1 = ax
+    
+    ax1.bar(np.arange(len(newCases)),newCases,label='New cases per day')
+    ax1.set_xlabel('Number of days after ' + date)
+    ax1.set_ylabel('New cases per day')
+    ax1.tick_params(axis='y')
+    
+    ax2 = ax1.twinx()
+    
+    ax2.plot(x,cumulative,color = 'red',label='Total number of cases')
+    ax2.set_ylabel('Total number of cases')
+    ax2.tick_params(axis='y', labelcolor='red')
+    # ax2.plot(np.log(x), np.log(fit[0] * x + fit[1]))
+    
+    
+fig2.subplots_adjust(wspace=0.8,hspace=0.4)
+fig2.suptitle('COVID-19 Number of cases in various countries \nDate : '+ confirmed.columns[-1])
 
 
+#%% Export the figures 
 
-
-
-
+today = confirmed.columns[-1]
+todayFormated = today[2:4] + '_' + today[0]
+fig1.savefig(todayFormated + '_normalisedComparisonWithItaly')
+fig2.savefig(todayFormated + '_numberOfCases')
 
